e00560af2c070ad5e73857bea88d75a3516f1670
admin <admin@SR-INT-16>
2025-07-31 11:41:41 +0530

    datatypes done

---
+++ Zoho-Assignments/src/Question2.java
@@ -0,0 +1,23 @@
+// Write a program in which, declare all data types like integer, double, float, long integer, character and byte data and print them.
+
+public class Question2 {
+    public static void main(String[] args) {
+
+        // all Data Types Declaration
+    	
+        int intVar = 100;
+        double doubleVar = 99.99;
+        float floatVar = 15.75f;
+        long longVar = 1234567890L;
+        char charVar = 'A';
+        byte byteVar = 120;
+        
+        System.out.println("Integer Value : " + intVar);
+        System.out.println("Double Value : " + doubleVar);
+        System.out.println("Float Value : " + floatVar);
+        System.out.println("Long Integer Value: " + longVar);
+        System.out.println("Character Value : " + charVar);
+        System.out.println("Byte Value : " + byteVar);
+    }
+}
+
